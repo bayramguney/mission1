66640e3f6deea5a46168f090ff4df55444c4ef22
TechnoStudy <info@techno.study>
2019-11-23 15:11:35 -0500

    day 37

--- src/day37/task/stringBuilder/Task1.java
+++ src/day37/task/stringBuilder/Task1.java
@@ -1,4 +1,12 @@
 package day37.task.stringBuilder;
 
+import java.util.ArrayList;
+
+// you have list of student names
+// you need to return a string which is first letter of their names
 public class Task1 {
+
+    public String getLetters(ArrayList<String> names){
+
+    }
 }
