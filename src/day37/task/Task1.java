af76e27338ac0c57427460397723fc96f0cf674a
TechnoStudy <info@techno.study>
2019-11-23 12:26:18 -0500

    day 37

--- src/day37/task/Task1.java
+++ src/day37/task/Task1.java
@@ -1,8 +1,10 @@
 package day37.task;
 
 // create class with 3 attribute
-// and one the attributes should be class also
+// and one the attributes should be class also which has 2 attributes there
 // print all attributes in task1 class
+
+// Hint: like example with User & Profile
 
 public class Task1 {
 }
