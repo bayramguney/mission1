51b3440eb88df5cd4fd450127d66e6057a412fbc
TechnoStudy <info@techno.study>
2019-11-24 14:42:44 -0500

    day 38 static added

--- src/day38/task/Task1.java
+++ src/day38/task/Task1.java
@@ -6,5 +6,20 @@ package day38.task;
 // HINT: create class Counter with attribute [int count = 0]
 // increment value of count in default constructor of Counter
 
+class Counter{
+    //write you code here
+    
+    //end of code
+}
 public class Task1 {
+
+    public static void main(String[] args) {
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+    }
+
+    // 1
+    // 2
+    // 3
 }
