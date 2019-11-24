34ad8711d5f9f5e27d55d8e406f6452d51d69f3b
TechnoStudy <info@techno.study>
2019-11-24 11:14:52 -0500

    day 38

--- src/day38/AccessModifiersAndNonAccessModifiersIntroOOP.java
+++ src/day38/AccessModifiersAndNonAccessModifiersIntroOOP.java
@@ -2,10 +2,13 @@ package day38;
 
 public class AccessModifiersAndNonAccessModifiersIntroOOP {
     // Access modifiers, Non access modifiers, Intro OOP
-    // private
+
+    // Access modifiers - allows you to access/view/visit a class/attribute/method
     // public
+    // private
     // protected
     // default package
+
     // static
     // final
     // abstract...
