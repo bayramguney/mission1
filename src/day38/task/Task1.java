8ae330796c92bbc558fe6173e2b699befda6cfe8
TechnoStudy <info@techno.study>
2019-11-24 15:13:09 -0500

    day 38 static added

--- src/day38/task/Task1.java
+++ src/day38/task/Task1.java
@@ -2,23 +2,27 @@ package day38.task;
 
 
 // Write a program that will count every time when you create an object and print it
-// HINT: create class Counter with attribute [int count = 0]
-// increment value of count in default constructor of Counter
+// HINT:
+//[✓] 1. create class Counter
+//[✓] 2. add attribute [int count = 0]
+//[✓] 3. increment value of count in default constructor of Counter class
+//[✓] 4. print count in default constructor of Counter class
 
-class Counter{
-    //write you code here
+class Counter {
+    public static int count = 0;
 
-    //end of code
+    public Counter() {
+        System.out.println("Default Constructor");
+        System.out.println("Count is: " + count);
+    }
 }
-public class Task1 {
 
+public class Task1 {
     public static void main(String[] args) {
         Counter c1 = new Counter();
-        Counter c2 = new Counter();
-        Counter c3 = new Counter();
-    }
+        c1.count++;
 
-    // 1
-    // 2
-    // 3
+        System.out.println("--------------------------");
+        Counter c2 = new Counter();
+    }
 }
