960717b37207bdc45fdcb625257a9a0b0762451e
TechnoStudy <info@techno.study>
2019-11-24 14:40:29 -0500

    day 38 static added

---
+++ src/day38/task/Task1.java
@@ -0,0 +1,10 @@
+package day38.task;
+
+
+// write a program that will count every time when you create an object
+
+// HINT: create class Counter with attribute [int count = 0]
+// increment value of count in default constructor of Counter
+
+public class Task1 {
+}
