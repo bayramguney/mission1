7fe8453c0d06a8c017924e33a5a1f14660d72862
TechnoStudy <info@techno.study>
2019-11-30 15:16:53 -0500

    day 42 Inheritance

--- src/day42/Inheritance.java
+++ src/day42/Inheritance.java
@@ -1,4 +1,17 @@
 package day42;
 
+import day42.example.inheritance.House;
+import day42.example.inheritance.SingleHouse;
+import day42.example.inheritance.Warehouse;
+
 public class Inheritance {
+    public static void main(String[] args) {
+        SingleHouse sh = new SingleHouse();
+        SingleHouse sh2 = new SingleHouse();
+
+        Warehouse wh = new Warehouse(1, 1000.10);
+
+        House house1 = new SingleHouse();
+        House house2 = new Warehouse(0,0);
+    }
 }
