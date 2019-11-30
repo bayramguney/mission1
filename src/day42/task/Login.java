e9da8c882d413361654efe0b92f5f4c9ca9d79de
TechnoStudy <info@techno.study>
2019-11-30 14:17:08 -0500

    day 42: task1

--- src/day42/task/Login.java
+++ src/day42/task/Login.java
@@ -19,31 +19,24 @@ public class Login {
     }
 
     public void setPassword(String password) {
-        // toCharArray
-        // if letter is a-z or A-Z
-        // then we have letter
-        // checking for at least one letter
-        boolean hasLetter = false;
-        for (char letter : password.toCharArray()) {
-            if ((letter >= 97 && letter <= 122) // letter in a-z
-                    || (letter >= 65 && letter <= 90)) { //letter in A-Z
-                hasLetter = true;
-            }
-
-//            if ((letter >= 'a' && letter <= 'z')) { // letter in a-z
-//                hasLetter = true;
-//            } else if (letter >= 'A' && letter <= 'Z') { //letter in A-Z
-//                hasLetter = true;
-//            }
-        }
-
-        //check has at least one number
-
+        boolean hasLetter = hasAtLeastOneLetter(password);
         if (hasLetter) {
             this.password = password;
         } else {
             this.password = null;
         }
+    }
+
+
+    private boolean hasAtLeastOneLetter(String password) {
+        boolean hasLetter = false;
+        for (char letter : password.toCharArray()) {
+            if ((letter >= 'a' && letter <= 'z') // letter in a-z
+                    || (letter >= 'A' && letter <= 'Z')) { //letter in A-Z
+                hasLetter = true;
+            }
+        }
+        return hasLetter;
     }
 
     public boolean hasRememberMe() {
