80eaf9a744cea11c97031219a459a25454319481
TechnoStudy <info@techno.study>
2019-12-01 15:05:35 -0500

    day 43 Override

--- src/day43/task/Task3.java
+++ src/day43/task/Task3.java
@@ -1,13 +1,25 @@
 package day43.task;
 
+import java.util.List;
+
+
+
+
+
 public class Task3 {
-    //Create class TesterBaseSalary
-    // add method getSalary which will return 100,000 as salary
+    // Create class TesterBaseSalary
+    // add method getSalary which will
+    // return $100,000 as salary
 
     // create subclasses JuniorTesterSalary, MiddleTesterSalary, SeniorTesterSalary
-    // junior take 50,000 less than base
-    // middle gets 10,000 more than base
-    // senior get 100,000 more than base
+    // junior take $50,000 less than base
+    // middle gets $10,000 more than base
+    // senior get $100,000 more than base
 
     // using polymorphism create methods in subclasses
+    // 110,000  for middle
+
+    // Hint: use super keyword to call method in base/parent class
+
+
 }
