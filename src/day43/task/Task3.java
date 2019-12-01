e3bae2e95ce65773406c988dcb0d8875c9faf83d
TechnoStudy <info@techno.study>
2019-12-01 15:33:54 -0500

    day 43 Override

--- src/day43/task/Task3.java
+++ src/day43/task/Task3.java
@@ -30,12 +30,9 @@ public class Task3 {
     @Test
     public void testBaseSalary() {
         TesterBaseSalary s = new TesterBaseSalary();
-//        Assert.assertEquals(100_000, s.getSalary());
-
         int actual = s.getSalary();
         int expected = 100_000;
-
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(expected, actual); //        Assert.assertEquals(100_000, s.getSalary());
     }
 
     @Test
@@ -48,14 +45,35 @@ public class Task3 {
     public void testMiddle() {
         MiddleTesterSalary m = new MiddleTesterSalary();
         Assert.assertEquals(110_000, m.getSalary());
-
     }
 
     @Test
     public void testSenior() {
         SeniorTesterSalary m = new SeniorTesterSalary();
         Assert.assertEquals(200_000, m.getSalary());
+    }
+
+    @Test
+    public void testSumOfThreeJuniorSalary() {
+        // create 3 junior testers, sum their salary, and check if you have $150,000 in total
+//        TesterBaseSalary j1 = new JuniorTesterSalary();
+//        TesterBaseSalary j2 = new JuniorTesterSalary();
+//        TesterBaseSalary j3 = new JuniorTesterSalary();
+//        int actual = j1.getSalary() + j2.getSalary() + j3.getSalary();
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            TesterBaseSalary junior = new JuniorTesterSalary();
+            sum += junior.getSalary();
+        }
+        int actual = sum;
+
+        Assert.assertEquals(150_000, actual);
 
     }
 
+    @Test
+    public void testSumOfNMiddleSalary() {
+        // create N middle testers, sum their salary, and check if you have N*$ 110_000 in total
+    }
 }
