6263243178720b81c09381286586967cf9be8e1c
TechnoStudy <info@techno.study>
2019-12-01 16:13:55 -0500

    day 43 Override

---
+++ src/day43/example/overriding/Override4.java
@@ -0,0 +1,15 @@
+package day43.example.overriding;
+
+
+class A{
+    public static void m(){
+
+    }
+}
+
+class B extends A{
+
+
+}
+public class Override4 {
+}
---
+++ src/day43/task/Task5.java
@@ -0,0 +1,6 @@
+package day43.task;
+
+public class Task5 {
+    /// create two more vegetable
+    /// add check for onion also
+}
