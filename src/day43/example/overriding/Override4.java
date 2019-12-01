a73aaa5e9302704b7ba17a2f0248ffbc5d1cd878
TechnoStudy <info@techno.study>
2019-12-01 16:27:38 -0500

    day 43 Override

--- src/day43/example/overriding/Override4.java
+++ src/day43/example/overriding/Override4.java
@@ -1,19 +1,26 @@
 package day43.example.overriding;
 
 
-class Person {
-    public static void walk(){
+import day43.example.instanceOf.Onion;
 
+class Person {
+    public void run() {
+
+    }
+
+    public static void walk() {
     }
 }
 
 class Teacher extends Person {
+    //    @Override - cannot override because of static
+    public static void walk() {
+    }
 
-//    @Override
-//    public static void walk(){
-//
-//    }
+    @Override
+    public void run() {
 
+    }
 }
 
 public class Override4 {
