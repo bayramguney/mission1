bc4a2c140bac3791adb2aef52d765aa9d0371bcd
TechnoStudy <info@techno.study>
2019-12-01 11:17:00 -0500

    day 43 Overloading

--- src/day43/task/Task.java
+++ src/day43/task/Task.java
@@ -1,5 +1,17 @@
 package day43.task;
 
+import org.junit.Test;
+
 public class Task {
-    //
+    //create class Multiplier
+    //create method which multiply 2 integers
+    //create method which multiply 3 integers
+    //create method which multiply a list of integers
+    //create method which multiply 2 double
+    //and test them using junit
+
+    @Test
+    public void test(){
+
+    }
 }
