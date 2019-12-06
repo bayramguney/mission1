3db0b7e9dac5491f7f829bf3e86f036f0dd49628
TechnoStudy <info@techno.study>
2019-12-05 21:07:29 -0500

    day 44 OOP

--- src/day41/homework/TestClass.java
+++ src/day41/homework/TestClass.java
@@ -1,42 +1,125 @@
 package day41.homework;
 
-//IQ: what is static?, how is it used, why we have to use.
-//compiler will go to static first. Debug and you will see.
 public class TestClass {
     public static void main(String[] args) {
-        C c = new C ();
-        StaticInstanceExample s = new StaticInstanceExample ();
-        //s.
-        //how is it used.
-        StaticInstanceExample.val = 45;
-
-        //why we have to use?
-        //when you need to share it among other classes or objects,
-        //without create an instance on a new class.
-    }
-}
-class StaticInstanceExample{
-    static int val = 42;
-}
-class A {
-
-    static {
-        System.out.println ( "THIRD" );
+        Y y = new Y ();
     }
 }
 
-class B extends A {
-    static {
-        System.out.println ( "SECOND" );
+class X {
+    public X(int i) {
+        System.out.println ( 1 );
+    }
+
+    public X() {
+        System.out.println ( 1 );
     }
 }
 
-class C extends B {
-    static {
-        System.out.println ( "FIRST" );
+class Y extends X {
+    public Y() {
+        super ();
+        System.out.println ( 2 );
     }
 }
+//
+//public class TestClass {
+//    public static void main(String[] args) {
+////        Yellow y = new Yellow ();
+////        y.staticMethod ();
+//        Yellow.staticMethod ();
+//    }
+//}
+//
+//class Xray {
+//    static void staticMethod() {
+//        System.out.println ( "Class  Xray" );
+//    }
+//}
+//
+//class Yellow extends Xray {
+//    static void staticMethod() {
+//        System.out.println ( "Class Yellow" );
+//    }
+//}
 
+//public class TestClass5 {
+//    public static void main(String[] args) {
+//        ConstructorExample ce = new ConstructorExample ();
+//        C c = new C ();
+//    }
+//}
+//
+//class A {
+//    public A() {
+//        System.out.println ( "Class A Constructor" );
+//    }
+//}
+//
+//class B extends A {
+//    public B() {
+//        System.out.println ( "Class B Constructor" );
+//    }
+//}
+//
+//class C extends B {
+//    public C() {
+//        System.out.println ( "Class C Constructor" );
+//    }
+//}
+//
+////constructor is used to initialize objects.
+////it will be invoked/called when we create with new
+//class ConstructorExample {
+//
+//    public ConstructorExample(int key) {
+//
+//    }
+//    public ConstructorExample() {
+//        val = 42;
+//        System.out.println ( "Constructor called" );
+//    }
+//    public int val;
+//}
+
+
+////IQ: what is static?, how is it used, why we have to use.
+////compiler will go to static first. Debug and you will see.
+//public class TestClass4 {
+//    public static void main(String[] args) {
+//        C c = new C ();
+//        StaticInstanceExample s = new StaticInstanceExample ();
+//        //s.
+//        //how is it used.
+//        StaticInstanceExample.val = 45;
+//
+//        //why we have to use?
+//        //when you need to share it among other classes or objects,
+//        //without create an instance on a new class.
+//    }
+//}
+//class StaticInstanceExample{
+//    static int val = 42;
+//}
+//class A {
+//
+//    static {
+//        System.out.println ( "THIRD" );
+//    }
+//}
+//
+//class B extends A {
+//    static {
+//        System.out.println ( "SECOND" );
+//    }
+//}
+//
+//class C extends B {
+//    static {
+//        System.out.println ( "FIRST" );
+//    }
+//}
+//
 
 //public class TestClass3 {
 //    public static void main(String[] args) {
@@ -121,18 +204,18 @@ class C extends B {
 //    }
 //}
 
-
-interface X {
-    //Class X Members
-}
-
-interface Y {
-    //Class Y Members
-}
-
-class Z implements X, Y {
-    //Class Z Members
-}
+//
+//interface X {
+//    //Class X Members
+//}
+//
+//interface Y {
+//    //Class Y Members
+//}
+//
+//class Z implements X, Y {
+//    //Class Z Members
+//}
 //interview question.
 //can you inherit multiple classes: NO
 //can you inherit multiple interfaces : Yes
