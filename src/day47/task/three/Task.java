9b14c2a6f80e7e1275cb0418ffe59fb90dac378b
TechnoStudy <info@techno.study>
2019-12-07 15:26:29 -0500

    day 47

---
+++ src/day47/task/three/Task.java
@@ -0,0 +1,9 @@
+package day47.task.three;
+
+public class Task {
+    // add files below to connect, read, save and update date using ex3
+    // 1. TxtFile
+    // 2. PowerPointFile
+    // 3. DMGFile
+    // 4. EXEFile
+}
