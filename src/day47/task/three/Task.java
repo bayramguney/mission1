8435d7de8f1c8653e6607495d671239aeda60013
TechnoStudy <info@techno.study>
2019-12-07 15:33:41 -0500

    day 47

--- src/day47/task/three/Task.java
+++ src/day47/task/three/Task.java
@@ -4,6 +4,6 @@ public class Task {
     // add files below to connect, read, save and update date using ReadFile in ex3
     // 1. TxtFile
     // 2. PowerPointFile
-    // 3. DMGFile
-    // 4. EXEFile
+    // 3. DMGFile //Definition of: DMG. DMG. (Disk iMaGe) The file format used in the Mac for distributing software.
+    // 4. EXEFile // EXE is a file extension for an executable file format.
 }
