862ad5e6ca656bbb82e207f4400012759d600726
TechnoStudy <info@techno.study>
2019-12-07 14:27:31 -0500

    day 47

--- src/day47/task/two/Task2.java
+++ src/day47/task/two/Task2.java
@@ -1,11 +1,10 @@
 package day47.task.two;
 
+// open-ended task, add extra class, interface, abstract class if you want
 public class Task2 {
+    //part1
     // create interface Vehicle
     // add three types of vehicle class: Car, Plain, Train
-
-    // open-ended task, add extra class, interface, abstract class if you want
-
 
     //part2
     // add interfaces for flying vehicles and vehicles in sea
