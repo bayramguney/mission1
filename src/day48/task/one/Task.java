d108cdc83433bd6051906a1b8fb9fad07535a5db
TechnoStudy <info@techno.study>
2019-12-08 11:32:59 -0500

    day 48

--- src/day48/task/one/Task.java
+++ src/day48/task/one/Task.java
@@ -2,5 +2,11 @@ package day48.task.one;
 
 public class Task {
     // create interface animal
-    // create 3 animals implementing animal interface
+    // create 3 animals classes implementing animal interface
+
+    //part1
+
+    //part2
+
+    //part3
 }
