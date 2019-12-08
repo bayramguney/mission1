ff1750a724c4014f64fb48d97b97bc08299ea4ec
TechnoStudy <info@techno.study>
2019-12-08 11:46:09 -0500

    day 48

--- src/day48/task/one/Task.java
+++ src/day48/task/one/Task.java
@@ -5,8 +5,18 @@ public class Task {
     // create 3 animals classes implementing animal interface
 
     //part1
+    // add method sound, print different sounds for animals
 
     //part2
+    // create class Zoo.java
+    // create 10 animal object
+    // print sounds of your animals
 
     //part3
+    // if you have one special animal for ex. Snake,
+    // print that it is special animal
+    // hint: instanceof
+
+    //part4
+
 }
