93280106c623bfdd6fd7f8464bcc20071884a289
TechnoStudy <info@techno.study>
2019-12-10 21:45:22 -0500

    day 50

--- src/day50/SeleniumIntro.java
+++ src/day50/SeleniumIntro.java
@@ -4,13 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumIntro {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty( "webdriver.gecko.driver", "D:\\TechnoStudy\\Selenium\\FirefoxDriver\\geckodriver.exe" );
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+        WebDriver driver = new ChromeDriver();
         driver.get( "https://www.seleniumeasy.com/test/input-form-demo.html" );
         driver.manage().window().maximize();
         Thread.sleep( 1000 );
